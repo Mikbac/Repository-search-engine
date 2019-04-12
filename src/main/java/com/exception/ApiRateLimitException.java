6c2bcc17f33c5270b7e93422ce880756aace1a26
MikBac <mbachorz1997@gmail.com>
2019-04-13 01:41:22 +0200

    updated ApiRateLimitException

--- src/main/java/com/exception/ApiRateLimitException.java
+++ src/main/java/com/exception/ApiRateLimitException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ApiRateLimitException extends RuntimeException{
+public class ApiRateLimitException extends RuntimeException {
     public ApiRateLimitException() {
-        super("API rate limit!");
+        super("GitHub API rate limit!");
     }
 }
